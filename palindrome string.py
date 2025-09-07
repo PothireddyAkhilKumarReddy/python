725a8257e46bcbcca5e667c735d4ad7204c10598
PothireddyAkhilKumarReddy <2300031067@kluniversity.in>
2025-09-07 16:53:18 +0530

    palindrome string

--- palindrome string.py
+++ palindrome string.py
@@ -1,3 +1,17 @@
-# slicing
+# # slicing
+# n=input()
+# print(n==n[::-1])
+# without slicing
 n=input()
-print(n==n[::-1])
+a=len(n)
+i=0
+flag=0
+while a>0:
+    if(n[a-1]==n[i]):
+        i=i+1
+        flag = flag + 1
+    a = a - 1
+if(flag==len(n)):
+    print('palindrome')
+else:
+    print('not palindrome')
