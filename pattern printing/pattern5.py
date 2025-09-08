46a3f82a3a7c172f096c0b0dd40dcb6e43407ba7
PothireddyAkhilKumarReddy <2300031067@kluniversity.in>
2025-09-08 22:07:15 +0530

    pattern11

--- pattern printing/pattern5.py
+++ pattern printing/pattern5.py
@@ -1,5 +1,10 @@
+#double loop
+# n=int(input())
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print(i,end=" ")
+#     print()
+#single loop
 n=int(input())
 for i in range(1,n+1):
-    for j in range(1,i+1):
-        print(i,end=" ")
-    print()
+    print((str(i)+" ")*i)
